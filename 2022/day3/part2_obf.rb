16e91be6f66abefa02e083ed84c3d320f8d8ed82
HiDoYa <hiroyagojo@gmail.com>
2022-12-04 14:55:10 -0800

    Day 3 obf

--- 2022/day3/part2_obf.rb
+++ 2022/day3/part2_obf.rb
@@ -2,4 +2,4 @@ p File.open("input.txt").readlines.map(&:chomp)
 .map{|x| x.split('').uniq.sort}.each_slice(3)
 .map{|l1, l2, l3| (l1+l2+l3).sort}
 .map{|a| a.select{|c| a.count(c) == 3}[0] }
-.map{|c| c.ord < 'a'.ord ? (c.ord - 'A'.ord + 1) + 26 : c.ord - 'a'.ord + 1}.sum
+.map{|c| c.ord - (c.ord < 97 ? 38 : 96)}.sum
