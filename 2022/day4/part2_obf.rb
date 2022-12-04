2b43b626dba16bdbde933f32b77a029465d659f5
HiDoYa <hiroyagojo@gmail.com>
2022-12-04 15:14:41 -0800

    Day 4 obf

--- 2022/day4/part2_obf.rb
+++ 2022/day4/part2_obf.rb
@@ -1,5 +1,5 @@
 p File.open("input.txt").readlines.map(&:chomp).map{ |l| 
     l.split(',').map{|x| x.split('-').map(&:to_i)}
 }.flatten(2).each_slice(4).map{ |a,b,c,d|
-    (a <= c && b >= c) || (c <= a && d >= a) || (a <= d && b >= d) || (c <= b && d >= b) ? 1 : 0
+    (a <= c && c <= b) || (c <= b && b <= d) ? 1 : 0
 }.sum
