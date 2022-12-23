d90d7e5f1b6a1f72d1a58b23d73c31798f29bfce
HiDoYa <hiroyagojo@gmail.com>
2022-12-22 22:28:28 -0800

    Day 12

--- 2022/day12/part2.rb
+++ 2022/day12/part2.rb
@@ -8,14 +8,13 @@ def get_distances(distances, index)
     distances[index[0]][index[1]]
 end
 
-def get_adjacent(indices, current, heightmap, visited, to_visit, height, width)
+def get_adjacent(index, current, heightmap, visited, to_visit, height, width)
     vals = [
-        [indices[0], indices[1] - 1],
-        [indices[0], indices[1] + 1],
-        [indices[0] - 1, indices[1]],
-        [indices[0] + 1, indices[1]],
+        [index[0], index[1] - 1],
+        [index[0], index[1] + 1],
+        [index[0] - 1, index[1]],
+        [index[0] + 1, index[1]],
     ]
-
 
     vals.
         select{ |n| n[0] >= 0 and n[1] >= 0}.
@@ -25,8 +24,8 @@ def get_adjacent(indices, current, heightmap, visited, to_visit, height, width)
         select{ |n| get_height(heightmap, n).ord + 1 >= get_height(heightmap, current).ord }
 end
 
-start_indices = [0, 0]
-s_indices = [0, 0]
+start_index = [0, 0]
+s_index = [0, 0]
 
 height = heightmap.length
 width = heightmap[0].length
@@ -35,27 +34,27 @@ distances = Array.new(height) { Array.new(width) { 0 } }
 heightmap.each_with_index do |line, line_index|
     if line.include? "S"
         chr_index = line.split("").find_index("S")
-        start_indices = [line_index, chr_index]
+        start_index = [line_index, chr_index]
     end
 
     if line.include? "E"
         chr_index = line.split("").find_index("E")
-        start_indices = [line_index, chr_index]
+        start_index = [line_index, chr_index]
     end
 end
 
-heightmap[start_indices[0]][start_indices[1]] = 'z'
-heightmap[s_indices[0]][s_indices[1]] = 'a'
+heightmap[start_index[0]][start_index[1]] = 'z'
+heightmap[s_index[0]][s_index[1]] = 'a'
 
-distances[start_indices[0]][start_indices[1]] = 0
 visited = []
-to_visit = [start_indices]
+to_visit = [start_index]
 
 while not to_visit.empty?
     p to_visit.length
+    # Sort by distances (heap would be better here)
     to_visit.sort_by{ |indices| get_distances(distances, indices) }
 
-    # Get next node
+    # Pop next node
     current = to_visit[0]
     to_visit = to_visit[1..]
     visited.append(current)
